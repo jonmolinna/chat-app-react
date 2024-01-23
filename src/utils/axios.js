1612b3a2135249455444f94002f8b9ee23caf443
jonmolinna <jmolina2624@gmail.com>
2024-01-23 06:55:55 -0500

    Add EndPoint

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = 'http://localhost:9000/api/';
+// const BASE_URL = 'http://localhost:9000/api/';
+const BASE_URL = 'https://chat-app-nodejs-rgvg.onrender.com/api';
 
 export default axios.create({
     baseURL: BASE_URL,
